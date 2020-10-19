20b3842bcb4a3cf85bcd4d142062d7777c41d61e
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2020-10-19 12:45:57 +1100

    M  wp-config.php M  wp-config/wp-config-footer.php A  wp-config/wp-config.example.php

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,22 @@
 <?php
 
+/*
+ * Dynamically load the correct wp-config file.
+ * if 'HTTP_HOST' is not defined, assume wp is running in the terminal with wp-cli & load the localhost config
+ * if 'HTTP_HOST' contains 'localhost', load the localhost config
+ * otherwise, load production config
+ * */
+if (
+	(!array_key_exists('HTTP_HOST', $_SERVER)) ||
+	(
+		(array_key_exists('HTTP_HOST', $_SERVER)) &&
+		(strpos($_SERVER['HTTP_HOST'], 'localhost') >= 0)
+	)
+) {
+	require_once(dirname(__FILE__) . '/wp-config/wp-config-localhost.php');
 
-require_once(dirname(__FILE__) . '/wp-config/wp-config-localhost.php');
+} else {
+	require_once(dirname(__FILE__) . '/wp-config/wp-config-production.php');
+}
 
 require_once(dirname(__FILE__) . '/wp-config/wp-config-footer.php');
--- wp-config/wp-config-footer.php
+++ wp-config/wp-config-footer.php
@@ -1,22 +1,5 @@
 <?php
 
-
-/**
- * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
- * If you want to access only from an specific domain, you can modify them. For example:
- *  define('WP_HOME','http://example.com');
- *  define('WP_SITEURL','http://example.com');
- *
- */
-
-if ( defined( 'WP_CLI' ) ) {
-	$_SERVER['HTTP_HOST'] = 'localhost';
-}
-
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
-define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
-
-
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
@@ -24,8 +7,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
-
-define('WP_TEMP_DIR', '/home/user/workspace/software/bitnami-lamp/apps/wordpress/tmp');
 
 
 //  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
