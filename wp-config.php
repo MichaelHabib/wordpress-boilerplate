c561d2eca534b8e94c012ca4bf03e8b913298226
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2020-10-24 15:40:15 +1100

    modified config loading condition

--- wp-config.php
+++ wp-config.php
@@ -6,12 +6,10 @@
  * if 'HTTP_HOST' contains 'localhost', load the localhost config
  * otherwise, load production config
  * */
+
 if (
-	(!array_key_exists('HTTP_HOST', $_SERVER)) ||
-	(
-		(array_key_exists('HTTP_HOST', $_SERVER)) &&
-		(strpos($_SERVER['HTTP_HOST'], 'localhost') >= 0)
-	)
+	(array_key_exists('HTTP_HOST', $_SERVER)) &&
+	(strpos($_SERVER['HTTP_HOST'], 'localhost') !== false)
 ) {
 	require_once(dirname(__FILE__) . '/wp-config/wp-config-localhost.php');
 
@@ -19,4 +17,29 @@ if (
 	require_once(dirname(__FILE__) . '/wp-config/wp-config-production.php');
 }
 
-require_once(dirname(__FILE__) . '/wp-config/wp-config-footer.php');
+
+/** Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+	define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+///** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/general/apps/wordpress/troubleshooting/xmlrpc-and-pingback/
+
+if (!defined('WP_CLI')) {
+	// remove x-pingback HTTP header
+	add_filter('wp_headers', function ($headers) {
+		unset($headers['X-Pingback']);
+		return $headers;
+	});
+	// disable pingbacks
+	add_filter('xmlrpc_methods', function ($methods) {
+		unset($methods['pingback.ping']);
+		return $methods;
+	});
+	add_filter('auto_update_translation', '__return_false');
+}
+
