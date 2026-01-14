63925b057f7cbb8ac823a642656a2c0de77f762e
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2026-01-14 12:39:27 +1100

    minor updates with better example

--- wp-config.php
+++ wp-config.php
@@ -1,12 +1,11 @@
 <?php
 
-/*
+/**
  * Dynamically load the correct wp-config file.
  * if 'HTTP_HOST' is not defined, assume wp is running in the terminal with wp-cli & load the localhost config
  * if 'HTTP_HOST' contains 'localhost', load the localhost config
  * otherwise, load production config
  * */
-
 
 if (
     (array_key_exists('HTTP_HOST', $_SERVER)) &&
--- wp-config/wp-config.example.php
+++ wp-config/wp-config.example.php
@@ -18,40 +18,25 @@
  * @package WordPress
  */
 
-//define('WP_SITEURL', 'http://example.com/');
-//define('WP_HOME', WP_SITEURL);
-//define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
-//
-////define('FS_METHOD', 'direct');
-//define('WP_DEBUG', true);
-//define('WP_DEBUG_DISPLAY', true);
-//define('WP_DEBUG_LOG', true);
-////define('WP_DISABLE_FATAL_ERROR_HANDLER', false);
-//define('SCRIPT_DEBUG', true);
-//define('DISALLOW_FILE_EDIT', true);
-//define('DISALLOW_FILE_MODS', true);
-
-//define('ALLOW_UNFILTERED_UPLOADS', true);
-
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', 'db');
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+//define( 'DB_USER', 'bn_wordpress' );
+define('DB_USER', 'db');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define('DB_PASSWORD', 'db');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', 'db');
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -62,14 +47,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY', '123');
+define('SECURE_AUTH_KEY', '123');
+define('LOGGED_IN_KEY', '123');
+define('NONCE_KEY', '123');
+define('AUTH_SALT', '123');
+define('SECURE_AUTH_SALT', '123');
+define('LOGGED_IN_SALT', '123');
+define('NONCE_SALT', '123');
 
 /**#@-*/
 
@@ -82,16 +67,42 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
 $table_prefix = 'wp_';
 
 /**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
+ * Custom Config
  */
-//define( 'WP_DEBUG', false );
 
+//define('WP_SITEURL', 'https://example.com');
+//define('WP_HOME', WP_SITEURL);
+//define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
+
+
+
+define('WP_DEBUG', TRUE);
+define('WP_DEBUG_LOG', TRUE);
+define('WP_DEBUG_DISPLAY', FALSE);
+define('WP_DISABLE_FATAL_ERROR_HANDLER', FALSE);
+
+define('SCRIPT_DEBUG', FALSE);
+define('CONCATENATE_SCRIPTS', TRUE);
+
+//define('DISALLOW_FILE_EDIT', TRUE);
+//define('DISALLOW_FILE_MODS', TRUE);
+//define('FS_METHOD', 'direct');
+
+//define('WP_TEMP_DIR', __DIR__.'/tmp');
+
+/* PHP memory limit for general application use */
+define( 'WP_MEMORY_LIMIT', '256M' );
+
+/* PHP memory limit for backend. */
+define( 'WP_MAX_MEMORY_LIMIT ', '512M' );
+
+
+/* PHP config changes (if not possible via CPANEL or php.ini */
+//@ini_set( 'log_errors', 0 );
+//@ini_set( 'display_errors', 0 );
+//@ini_set( 'upload_max_size' , '20M' );
+//@ini_set( 'post_max_size', '20M');
+//@ini_set( 'memory_limit', '800M' );
+
+
+/* That's all, stop editing! Happy publishing. */
